564d63c8c93255db74e341eecaaa83f15fd6a5d8
selina <selina.su@insighteye.com.tw>
2024-07-30 15:06:01 +0800

    fix: 註解使用繁體中文

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       template: { transformAssetUrls }
     }),
     quasar({
-      // sassVariables: '/loco/src/quasar-varibles.sass', // 如果有自定义 SASS 变量
-      cssAddon: true // 确保这里也启用了 cssAddon
+      cssAddon: true,
     })
   ]
 })
